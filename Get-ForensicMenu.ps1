f4997d5163ee6e31d7ba9f08e887454255caa100
Mark Sexton <65192130+marksexton1@users.noreply.github.com>
2020-09-11 21:33:30 +1000

    Update Get-ForensicMenu.ps1

--- Get-ForensicMenu.ps1
+++ Get-ForensicMenu.ps1
@@ -1,6 +1,6 @@
- #########################################
+###############################################
 ## Main Menu Function, add options as needed.##
-#########################################
+###############################################
  
 ## MENU options for the user to select. User MUST enter 'q' to exit from the Function ##
 function Get-Menu {
@@ -10,21 +10,22 @@ function Get-Menu {
     Write-Host -ForegroundColor  Green "================ $Title ================"
     Write-Host `n
     Write-Host -ForegroundColor DarkCyan "1: Ping Sweep Script."
-    Write-Host -ForegroundColor  DarkCyan "2: File Search and Hash."
-    Write-Host -ForegroundColor  DarkCyan "3: Enum Script."
-    Write-Host -ForegroundColor  DarkCyan "4: Remote Host Processes."
-    Write-Host -ForegroundColor  DarkCyan "5: Remote Host Connections."
-    Write-Host -ForegroundColor  DarkCyan "6: Remote Host Local Users/Groups (WILL ERROR ON DC)."
-    Write-Host -ForegroundColor  DarkCyan "7: Remote Host Services."
-    Write-Host -ForegroundColor  DarkCyan "8: Remote Host Scheduled Tasks."
-    Write-Host -ForegroundColor  DarkCyan "9: PSSession to remote host."
-    Write-Host -ForegroundColor  DarkCyan "10: Kill Process on remote host."
-    Write-Host -ForegroundColor  Red `n "Q: Press 'Q' to quit."
+    Write-Host -ForegroundColor DarkCyan "2: File Search and Hash."
+    Write-Host -ForegroundColor DarkCyan "3: Enum Script."
+    Write-Host -ForegroundColor DarkCyan "4: Remote Host Processes."
+    Write-Host -ForegroundColor DarkCyan "5: Remote Host Connections."
+    Write-Host -ForegroundColor DarkCyan "6: Remote Host Local Users/Groups (WILL ERROR ON DC)."
+    Write-Host -ForegroundColor DarkCyan "7: Remote Host Services."
+    Write-Host -ForegroundColor DarkCyan "8: Remote Host Scheduled Tasks."
+    Write-Host -ForegroundColor DarkCyan "9: PSSession to remote host."
+    Write-Host -ForegroundColor DarkCyan "10: Kill Process on remote host."
+    Write-Host -ForegroundColor DarkCyan "11: Memory Dump on a remote host."
+    Write-Host -ForegroundColor Red `n "Q: Press 'Q' to quit."
 } 
  
-###################################################
+##############################################################
 ## Functions that will be called upon below, within DO loop ##
-###################################################
+##############################################################
  
 ## FUNCTION 1: Ping Sweep a Class C network range, with the option of outputting your results to File ##
 Function Start-PingSweep {
@@ -44,6 +45,7 @@ Function Start-PingSweep {
                 }
             } | Out-File -Append -FilePath $Env:USERPROFILE\Desktop\PingResults.txt
         }
+        ## Run ping sweep and output to screen ##
         'N' {
             'Your results will be displayed below'
             1..254 | % {
@@ -61,6 +63,7 @@ Function Get-FilePathandMD5Hash {
     Write-Host `n
     $filecheckselection = Read-Host "Is the file on a remote host? (Y or N)"
     switch ($filecheckselection) {
+        ## Find file and hash, save to file ##
         'Y' {
             $remotehost = Read-Host "Please enter the remote host IP"
             $THip = [string]$remotehost
@@ -77,6 +80,7 @@ Function Get-FilePathandMD5Hash {
                 }
             }
         } 
+        ## Find file and hash, output to screen ##
         'N' {
             $filename = Read-Host "Please enter the filename (eg. hack.exe)"
             'Scan Running, Please wait....'
@@ -115,7 +119,7 @@ Function Get-Enumeration {
                     Write-Output "-----IPCONFIG-----"
                     ipconfig /all
                 } | Out-File -FilePath "$env:USERPROFILE\Desktop\$newdate Ipconfig $_.txt"
-                ## All TCP & UDP connections
+                ## All TCP & UDP connections ##
                 Invoke-Command -Credential $creds -ComputerName $_ -ScriptBlock {
                     Write-Output "`n`n-----NETSTAT-----"
                     netstat -ano
@@ -130,11 +134,16 @@ Function Get-Enumeration {
                     Write-Output "`n`n-----SERVICES-----"
                     Get-Service | Select-Object Status,Name
                 } | Out-File -FilePath "$env:USERPROFILE\Desktop\$newdate Services $_.txt"
+                ## View PowerShell History ##
+                Invoke-Command -Credential $creds -ComputerName $_ -ScriptBlock {
+                    Write-Output "`n`n-----PS CMD HISTORY-----"
+                    Get-History | Select-Object -Property *
+                } | Out-File -FilePath "$env:USERPROFILE\Desktop\$newdate History $_.txt"
                 ## All local user accounts & whether or not they are enabled/disabled ##
                 Invoke-Command -Credential $creds -ComputerName $_ -ScriptBlock {
                     Write-Output "`n`n-----LOCAL USERS-----"
                     $cmd = try {
-                        get-localuser;$true
+                        Get-LocalUser;$true
                     }
                     catch {
                         $false
@@ -224,7 +233,7 @@ Function Get-Enumeration {
     }
 }
  
-## FUNCTION 4: Kill a remote process 
+## FUNCTION 4: Kill a remote process ##
 Function Stop-RemoteProcess {
     Clear-Host
     Write-Host `n
@@ -250,9 +259,79 @@ Function Stop-RemoteProcess {
     } 
 }
  
-###################################################
+## FUNCTION 5: Get a Memory Dump from a remote machine ##
+Function Get-MemDump {
+    Clear-Host
+    Write-Host "    Ensure you have gone to the following WebSite and downloaded the Memory Dump executable:
+ 
+                          https://github.com/Velocidex/c-aff4/releases
+                          --------------------------------------------
+ 
+    WARNING: There will be a number of popup windows that may be in the background to PS!"
+    $DumpFile = Read-Host "Enter the file path of the Memory Dump program:`nEg: C:\Users\Bob\Desktop\winpmem.exe`n`n==>"
+    Clear-Host
+    [System.Windows.MessageBox]::Show('Enter the REMOTE MACHINE Name or IP Address - Click OK to Continue')
+    $RemoteComputer = Read-Host "Enter the remote machine name or IP address you wish to run DumpIt on:`nEg: WIN8-01   OR   192.168.1.10`n`n==>"
+    Clear-Host
+    [System.Windows.MessageBox]::Show('Enter the LOCAL MACHINE Name or IP Address - Click OK to Continue')
+    $LocalComputer = Read-Host "Enter the IP address of your local machine:`n`n==>"
+    Clear-Host
+    [System.Windows.MessageBox]::Show('Enter the credentials for the REMOTE MACHINE - Click OK to Continue')
+    $RemoteCreds = Get-Credential -Message "Enter the credentials for the REMOTE MACHINE!"
+    Clear-Host
+    [System.Windows.MessageBox]::Show('Enter the credentials for the LOCAL MACHINE - Click OK to Continue')
+    $LocalCreds = Get-Credential -Message "Enter the credentials for the LOCAL MACHINE"
+    
+    ## If the user enters an IP addresses, add it to the TrustedHosts value ##
+    IF ($RemoteComputer -match ("\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b")) {
+        $THip = [string]$RemoteComputer
+        Set-Item WSMan:\localhost\Client\TrustedHosts -Value $THip -Force
+    }
+    ELSE {            
+    }
+ 
+    ############################
+    ## Main Script Body below ##
+    ############################
+    
+    ## 1. Copy Memory Dump executable to remote machine via PSDrive Dump ##
+    New-PSDrive -Name Dump -PSProvider FileSystem -Root "\\$RemoteComputer\c$" -Credential $RemoteCreds
+    Copy-Item -Path $DumpFile -Destination Dump:
+ 
+    Invoke-Command -ComputerName $RemoteComputer -Credential $RemoteCreds -ScriptBlock {
+        ## 2. Run the Memory Dump executable on the remote machine ##
+        cmd.exe /c "c:\winpmem.exe -o c:\1.raw --format raw"
+        ## 3. Copy the RAW file back to the local machine ##
+        ## If the user enters an IP addresses, add it to the TrustedHosts value ##
+ 
+        IF ($using:LocalComputer -match ("\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b")) {
+            $THip = [string]$using:LocalComputer
+            Set-Item WSMan:\localhost\Client\TrustedHosts -Value $THip -Force
+        }
+        ELSE {            
+        }
+ 
+        $HomePC = New-PSSession -ComputerName $using:LocalComputer -Credential $using:LocalCreds
+        Copy-Item -Path 'C:\1.raw' -Destination 'c:\' -ToSession $HomePC
+ 
+        ## Remove the PSSession and process (wsmprovhost) from the remote machine ##
+        Clear-Item -Path 'WSMan:\localhost\Client\TrustedHosts' -Force
+        Get-PSSession | Remove-PSSession
+    }
+ 
+    ## 4. Remove ALL files from the remote machine and Clean Up ##
+    Remove-Item 'Dump:\1.raw' -Force
+    Remove-Item 'Dump:\winpmem.exe' -Force
+    Remove-PSDrive -Name Dump -Force
+    ## Return the TrustedHosts to $null ##
+    Clear-Item -Path 'WSMan:\localhost\Client\TrustedHosts' -Force
+}
+ 
+ 
+ 
+#############################################################
 ## Main Menu Loop which will only quit when 'q' is entered ##
-###################################################
+#############################################################
  
 do {
     Get-Menu
@@ -342,9 +421,8 @@ do {
                     }
                     Write-Host "----------$Sch---------- `n $state `n $exec `n"
                 } 
-                ## IF THE FOREACH LOOP ABOVE DOES NOT WORK, THIS IS A BACKUP COMMAND YOU CAN TRY
-                ## TO EXECUTE A FILE SEARCH AND A MD5 HASH OF IT
- 
+                ## IF THE FOREACH LOOP ABOVE DOES NOT WORK, THIS IS A BACKUP COMMAND YOU CAN TRY ##
+                 
                 ## $SchTask | % {
                     ## Write-Host "------------- $_ -------------"
                     ## Get-ScheduledTask -TaskName $_ | Select-Object -Property State -ExpandProperty Actions | 
@@ -368,6 +446,29 @@ do {
         '10' {
             Clear-Host
             Stop-RemoteProcess
+        }
+        ## Function 5 (above) ##
+        '11' {
+            Clear-Host
+            $TestIP = (Get-NetIPConfiguration | Where-Object {$_.IPv4DefaultGateway -ne $null -and $_.NetAdapter.Status -ne "Disconnected"}).IPv4Address.IPAddress
+            IF (Test-WSMan -ComputerName $TestIP -ErrorAction SilentlyContinue) {
+                Get-MemDump
+            }
+            ## IF PSRemoting is NOT installed, ENABLE it and run Function Get-MemDump ##
+            ## PSRemoting will also be removed if NOT originally installed ##
+            ELSE {
+                Enable-PSRemoting -Force
+                Get-MemDump
+                ## Disable PSRemoting, and rollback ALL changes from Enable-PSRemoting ##
+                Disable-PSRemoting -Force
+                Remove-Item -Path 'WSMan:\Localhost\listener\listener*' -Recurse
+                Get-Item -Path 'WSMan:\Localhost\listener\'
+                Stop-Service -Name 'WinRM' -Force
+                Get-Service -Name 'WinRM' | Select-Object Name,Status | Format-List
+                Set-NetFirewallRule -DisplayName 'Windows Remote Management (HTTP-In)' -Enabled False
+                Get-NetFirewallRule -DisplayName 'Windows Remote Management (HTTP-In)' | 
+                    Select-Object -Property DisplayName,Profile,Enabled | Format-List
+            }
         }
     }
     pause
